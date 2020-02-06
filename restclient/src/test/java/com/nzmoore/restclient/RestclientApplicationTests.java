f3d7a303f8310cf68e2e34a25958c17b621b87dc
Bill Moore <bill@nzmoore.com>
2020-02-05 21:21:36 -0500

    Initial template for restclient

---
+++ restclient/src/test/java/com/nzmoore/restclient/RestclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nzmoore.restclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
